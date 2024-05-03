faff39a427e4f0aa02f51324fd0aff22e78ae0a2
achorn <joshuaachorn@gmail.com>
2024-05-03 13:12:56 -0700

    sized canvas appropriately

---
+++ main.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", () => {
+  console.log("loaded");
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 500;
+  canvas.height = 500;
+});
