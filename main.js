eb05b55e0380283ea51e25fefba382c443ef6d0f
achorn <joshuaachorn@gmail.com>
2024-05-03 13:27:32 -0700

    created player class

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+import { Player } from "./player";
 window.addEventListener("load", () => {
   console.log("loaded");
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
   canvas.width = 500;
   canvas.height = 500;
+
+  class Game {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+      this.player = new Player(this);
+    }
+    update() {}
+    draw() {}
+  }
 });
