9040ca9f25f84a8c9f98af7ed69800fba54943c8
achorn <joshuaachorn@gmail.com>
2024-05-03 13:39:54 -0700

    created input class and added to game class

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import Player from "./player.js";
+import InputHandler from "./input.js";
 
 window.addEventListener("load", () => {
   console.log("loaded");
@@ -12,6 +13,7 @@ window.addEventListener("load", () => {
       this.width = width;
       this.height = height;
       this.player = new Player(this);
+      this.input = new InputHandler();
     }
     update() {
       this.player.update();
