0934ccb228adeee97716daeb6f9da48f086ab32f
achorn <joshuaachorn@gmail.com>
2024-05-03 15:36:02 -0700

    add game speed controller when updating state

--- playerStates.js
+++ playerStates.js
@@ -22,7 +22,7 @@ export class Sitting extends State {
   }
   handleInput(input) {
     if (input.includes("ArrowLeft") || input.includes("ArrowRight")) {
-      this.player.setState(states.RUNNING);
+      this.player.setState(states.RUNNING, 1);
     }
   }
 }
@@ -39,10 +39,10 @@ export class Running extends State {
   }
   handleInput(input) {
     if (input.includes("ArrowUp")) {
-      this.player.setState(states.JUMPING);
+      this.player.setState(states.JUMPING, 1);
     }
     if (input.includes("ArrowDown")) {
-      this.player.setState(states.SITTING);
+      this.player.setState(states.SITTING, 0);
     }
   }
 }
@@ -60,10 +60,10 @@ export class Jumping extends State {
   }
   handleInput(input) {
     if (this.player.vy > this.player.weight + 7) {
-      this.player.setState(states.FALLING);
+      this.player.setState(states.FALLING, 1);
     }
     if (this.player.onGround()) {
-      this.player.setState(states.RUNNING);
+      this.player.setState(states.RUNNING, 1);
     }
   }
 }
@@ -80,7 +80,7 @@ export class Falling extends State {
   }
   handleInput(input) {
     if (this.player.onGround()) {
-      this.player.setState(states.RUNNING);
+      this.player.setState(states.RUNNING, 1);
     }
   }
 }
