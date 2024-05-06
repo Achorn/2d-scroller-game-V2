a5baae8a91774418889006d5a931d44b4341ec67
achorn <joshuaachorn@gmail.com>
2024-05-06 12:58:05 -0700

    added emeny class and enemy sub classes

---
+++ enemies.js
@@ -0,0 +1,73 @@
+class Enemy {
+  constructor() {
+    this.frameX = 0;
+    this.frameY = 0;
+    this.fps = 20;
+    this.frameInterval = 1000 / this.fps;
+    this.frameTimer = 0;
+  }
+  update(deltaTime) {
+    this.x -= this.speedX;
+    console.log(this.x, this.speedX);
+    this.y += this.speedY;
+    if (this.frameTimer > this.frameInterval) {
+      this.frameTimer = 0;
+      if (this.frameX < this.maxFrame) this.frameX++;
+      else this.frameX = 0;
+    } else {
+      this.frameTimer += deltaTime;
+    }
+  }
+  draw(context) {
+    context.drawImage(
+      this.image,
+      this.frameX * this.width,
+      0,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+export class FlyingEnemy extends Enemy {
+  constructor(game) {
+    super();
+    this.game = game;
+    this.width = 60;
+    this.height = 44;
+    this.x = 50;
+    this.y = 50;
+    this.speedX = 2;
+    this.speedY = 0;
+    this.maxFrame = 5;
+    this.image = document.getElementById("enemy_fly");
+  }
+  update(deltaTime) {
+    super.update(deltaTime);
+  }
+  draw(context) {
+    super.draw(context);
+  }
+}
+
+export class GroundEnemy extends Enemy {
+  constructor(game) {
+    super();
+    this.game = game;
+    this.width = 60;
+    this.height = 44;
+  }
+}
+
+export class ClimbingEnemy extends Enemy {
+  constructor(game) {
+    super();
+    this.game = game;
+    this.width = 60;
+    this.height = 44;
+  }
+}
